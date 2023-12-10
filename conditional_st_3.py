069edee86d31c133ef3b171cc36c71b09534d2df
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2023-12-10 19:47:16 +0530

    Add files via upload

---
+++ conditional_st_3.py
@@ -0,0 +1,56 @@
+# conditional statement_3
+
+a = 2
+b = 2
+if a>b:
+    print("if a is greater than b print if")
+elif a<b:
+    print("if a is less than b print elif")
+elif a==b:
+    print("if a is equal than b print elif")
+else:
+    print("if not upper print else")
+
+a = 6
+b = 9
+if a>b:
+    print("if a is greater than b print if")
+elif a<b:
+    print("if a is less than b print elif")
+elif a==b:
+    print("if a is equal than b print elif")
+else:
+    print("if not upper print else")
+
+a = 55
+b = 42
+if a>b:
+    print("if a is greater than b print if")
+elif a<b:
+    print("if a is less than b print elif")
+elif a==b:
+    print("if a is equal than b print elif")
+else:
+    print("if not upper print else")
+
+a = 0
+b = 0
+if a>b:
+    print("if a is greater than b print if")
+elif a<b:
+    print("if a is less than b print elif")
+elif a==b:
+    print("if a is equal than b print elif")
+else:
+    print("if not upper print else")
+
+a = 100
+b = 200
+if a>b:
+    print("if a is greater than b print if")
+elif a<b:
+    print("if a is less than b print elif")
+elif a==b:
+    print("if a is equal than b print elif")
+else:
+    print("if not upper print else")
